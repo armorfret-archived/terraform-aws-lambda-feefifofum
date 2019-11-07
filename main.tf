a750e464fcfc0e5a93d6a2ae83086f304251da25
Les Aker <me@lesaker.org>
2019-11-07 09:18:00 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,90 @@
+module "apigw" {
+  source  = "armorfret/apigw-lambda/aws"
+  version = "0.1.0"
+
+  source_bucket  = "${var.lambda_bucket}"
+  source_version = "${var.lambda_version}"
+  function_name  = "feefifofum_${var.config_bucket}"
+
+  environment_variables = {
+    S3_BUCKET = "${var.config_bucket}"
+    S3_KEY    = "config.yaml"
+  }
+
+  access_policy_document = "${data.aws_iam_policy_document.lambda_perms.json}"
+
+  hostname = "${var.hostname}"
+}
+
+module "publish_user" {
+  source         = "armorfret/s3-publish/aws"
+  version        = "0.0.2"
+  logging_bucket = "${var.logging_bucket}"
+  publish_bucket = "${var.data_bucket}"
+}
+
+module "config_user" {
+  source         = "armorfret/s3-publish/aws"
+  version        = "0.0.2"
+  logging_bucket = "${var.logging_bucket}"
+  publish_bucket = "${var.config_bucket}"
+}
+
+resource "aws_sqs_queue" "data_queue" {
+  name   = "${var.data_queue}"
+  policy = "${data.aws_iam_policy_document.sqs_perms.json}"
+}
+
+data "aws_iam_policy_document" "lambda_perms" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.config_bucket}/*",
+      "arn:aws:s3:::${var.config_bucket}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.data_bucket}/*",
+      "arn:aws:s3:::${var.data_bucket}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+    ]
+
+    resources = [
+      "${aws_sqs_queue.data_queue.arn}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "lambda_version" {
+  description = "Version of the Lambda to use"
+  type        = "string"
+  default     = "v0.0.1"
+}
+
+variable "logging_bucket" {
+  description = "S3 bucket to use for bucket logging"
+  type        = "string"
+}
+
+variable "config_bucket" {
+  description = "S3 bucket to use for configuration"
+  type        = "string"
+}
+
+variable "data_bucket" {
+  description = "S3 bucket for messages"
+  type        = "string"
+}
+
+variable "data_queue" {
+  description = "SQS queue for messages"
+  type        = "string"
+}
+
+variable "lambda_bucket" {
+  description = "S3 bucket from which to read the Lambda ZIP"
+  type        = "string"
+}
+
+variable "hostname" {
+  description = "Hostname to use for the site"
+  type        = "string"
+}
